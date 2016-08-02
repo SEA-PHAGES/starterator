2ffb6284d60bc2fda5d01c52e9a175f66b75e7f7
SEA Faculty <seafaculty@SEA2016.(none)>
2016-08-02 16:59:33 -0400

    remove git conflict from phamgene comment section

--- starterator/phamgene.py
+++ starterator/phamgene.py
@@ -339,11 +339,7 @@ class PhamGene(Gene):
             starts of the genes are the same, and if the alignment gaps or not are the same
             (This is essentially, they would look the same on the graph output)
 
-<<<<<<< HEAD
-            TODO currenly only "is_equal" if the start coordinate is the identical base coordinate
-=======
             TODO currently only "is_equal" if the start coordinate is the identical base coordinate
->>>>>>> beta
             would make sense to compare the annotated start index
         """
         if self.alignment_start_site != other.alignment_start_site:
