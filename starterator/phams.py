c1785296282fb9d8d2426aff0fc9f93a6f80ac8a
Chris Shaffer <cdshaffer@gmail.com>
2016-12-06 14:14:14 -0600

    assign none to annot values if all genes are draft

--- starterator/phams.py
+++ starterator/phams.py
@@ -237,9 +237,9 @@ class Pham(object):
 
         genes_start_most_called = start_stats["called_starts"][most_called_start_index]
         start_stats["most_called_start"] = most_called_start_index
-
         start_stats["most_annotated_start"] = most_annot_start_index
-        if most_annot_start_index:
+
+        if most_annot_start_index is not None:
             genes_start_most_annot = start_stats["called_starts"][most_annot_start_index]
         else:
             genes_start_most_annot = None
@@ -282,34 +282,36 @@ class Pham(object):
                     possible_starts_coords.append((index, new_start))
                 gene.suggested_start["most_called"] = possible_starts_coords
 
-            if gene.gene_id in start_stats["possible"][most_annot_start_index]:
-                if gene.gene_id in genes_start_most_annot:
-                    # code below used for deprecated "suggested starts" list
-                    # if gene.orientation == 'F':  # only +1 for forward genes
-                    #     # genes where most annotated start is present and it is called as the start are "most_annotated"
-                    #     gene.suggested_start["most_annotated"] = (most_annot_start_index, gene.start + 1)
-                    # else:
-                    #     gene.suggested_start["most_annotated"] = (most_annot_start_index, gene.start)
-                    start_stats["most_annotated"].append(gene.gene_id)
-                else:
-                    # genes where most annotated start is present but the start is not the called start are "most_not_annotated
-                    start_stats["most_not_annotated"].append(gene.gene_id)
-                    # code below used for deprecated "suggested starts" list
-                    most_annot_alignment_index = self.total_possible_starts[most_annot_start_index - 1]
-                    suggested_start = gene.alignment_index_to_coord(
-                        most_annot_alignment_index)  # +1 issue dealt with in function
-                    gene.suggested_start["most_called"] = (most_annot_start_index, suggested_start)
+            if most_annot_start_index is not None:
+                if gene.gene_id in start_stats["possible"][most_annot_start_index]:
+                    if gene.gene_id in genes_start_most_annot:
+                        # code below used for deprecated "suggested starts" list
+                        # if gene.orientation == 'F':  # only +1 for forward genes
+                        #     # genes where most annotated start is present and it is called as the start are "most_annotated"
+                        #     gene.suggested_start["most_annotated"] = (most_annot_start_index, gene.start + 1)
+                        # else:
+                        #     gene.suggested_start["most_annotated"] = (most_annot_start_index, gene.start)
+                        start_stats["most_annotated"].append(gene.gene_id)
+                    else:
+                        # genes where most annotated start is present but the start is not the called start are "most_not_annotated
+                        start_stats["most_not_annotated"].append(gene.gene_id)
+                        # code below used for deprecated "suggested starts" list
+                        most_annot_alignment_index = self.total_possible_starts[most_annot_start_index - 1]
+                        suggested_start = gene.alignment_index_to_coord(
+                            most_annot_alignment_index)  # +1 issue dealt with in function
+                        gene.suggested_start["most_called"] = (most_annot_start_index, suggested_start)
 
-            else:
-                # genes where the most annotated start is NOT even present are no_most_annot
-                start_stats["no_most_annot"].append(gene.gene_id)
-                # Code below used for deprecated "suggested starts" list
-                # possible_starts_coords = []
-                # for start in gene.alignment_candidate_starts:
-                #     index = self.total_possible_starts.index(start) + 1
-                #     new_start = gene.alignment_index_to_coord(start) + 1
-                #     possible_starts_coords.append((index, new_start))
-                # gene.suggested_start["most_called"] = possible_starts_coords
+                else:
+                    # genes where the most annotated start is NOT even present are no_most_annot
+                    start_stats["no_most_annot"].append(gene.gene_id)
+                    # Code below used for deprecated "suggested starts" list
+                    # possible_starts_coords = []
+                    # for start in gene.alignment_candidate_starts:
+                    #     index = self.total_possible_starts.index(start) + 1
+                    #     new_start = gene.alignment_index_to_coord(start) + 1
+                    #     possible_starts_coords.append((index, new_start))
+                    # gene.suggested_start["most_called"] = possible_starts_coords
+
         self.stats["most_common"] = start_stats
         return start_stats
 
